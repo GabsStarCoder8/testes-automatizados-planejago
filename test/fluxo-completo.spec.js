4c8701398f1d2d237595376d97d3a8d5709c76f7
GabsStarCoder8 <gliberato2018@gmail.com>
2026-06-17 19:36:53 -0400

    deixa o fluxo-completo com o código limpo

--- test/fluxo-completo.spec.js
+++ test/fluxo-completo.spec.js
@@ -3,12 +3,9 @@ import { test, expect } from '@playwright/test';
 test('CT10: Fluxo-Completo Login > Criar receita > Criar Despesa > Ver Gráfico', async ({ page }) => {
   await page.goto('http://localhost:8000/');
   await page.getByRole('link', { name: 'Começar' }).click();
-  await page.getByRole('textbox', { name: 'Ex: João Silva' }).click();
   await page.getByRole('textbox', { name: 'Ex: João Silva' }).fill('Maria');
   await page.locator('input[name="data_nascimento"]').fill('2004-03-12');
-  await page.getByRole('textbox', { name: 'Ex: usuario@gmail.com' }).click();
   await page.getByRole('textbox', { name: 'Ex: usuario@gmail.com' }).fill('maria@gmail.com');
-  await page.locator('input[name="password"]').click();
   await page.locator('input[name="password"]').fill('senha12345678');
   await page.locator('input[name="password_confirmation"]').click();
   await page.locator('input[name="password_confirmation"]').fill('senha12345678');
@@ -20,11 +17,7 @@ test('CT10: Fluxo-Completo Login > Criar receita > Criar Despesa > Ver Gráfico'
   await page.getByRole('link', { name: 'Lançamentos' }).click();
   await page.locator('#btn-dropdown-lancamento').click();
   await page.getByRole('button', { name: 'Nova Receita' }).click();
-  await page.getByRole('textbox', { name: 'Descrição' }).click();
-  await page.getByRole('textbox', { name: 'Descrição' }).fill('Sal');
-  await page.getByRole('textbox', { name: 'Descrição' }).press('Dead');
   await page.getByRole('textbox', { name: 'Descrição' }).fill('Salário');
-  await page.getByRole('spinbutton', { name: 'Valor' }).click();
   await page.getByRole('spinbutton', { name: 'Valor' }).fill('1500');
   await page.getByRole('textbox', { name: 'Data da Criação' }).fill('2026-06-17');
   await page.locator('#frequencia_receita').selectOption('4');
@@ -35,9 +28,7 @@ test('CT10: Fluxo-Completo Login > Criar receita > Criar Despesa > Ver Gráfico'
 
   await page.locator('#btn-dropdown-lancamento').click();
   await page.getByRole('button', { name: 'Nova Despesa' }).click();
-  await page.getByRole('textbox', { name: 'Descrição Descrição' }).click();
   await page.getByRole('textbox', { name: 'Descrição Descrição' }).fill('Conta energia');
-  await page.getByRole('spinbutton', { name: 'Valor Valor' }).click();
   await page.getByRole('spinbutton', { name: 'Valor Valor' }).fill('350');
   await page.locator('#status').selectOption('false');
   await page.getByRole('textbox', { name: 'Data da Criação Data da Cria' }).fill('2026-06-17');
